abd7ca92a819b4978ca3c44ffcf9091818f3973b
amagai09 <amagai.ku@gmail.com>
2021-06-09 14:15:37 +0900

    Text Modify

--- js/Portfolo.js
+++ js/Portfolo.js
@@ -1,3 +1,4 @@
+//ナビゲーションメニューバーを操作時に、選択中の項目を暗転させる
 'use strict';
 const cover1= document.getElementById('cover1');
 const cover2= document.getElementById('cover2');
@@ -5,21 +6,21 @@ const cover3= document.getElementById('cover3');
 const cover4= document.getElementById('cover4');
 
 cover1.addEventListener('mouseover',()=>{
-    cover1.className='selecting';
+    cover1.className='Selecting';
 })
 cover1.addEventListener('mouseleave',()=>{
     cover1.className="";
 })
 
 cover2.addEventListener('mouseover',()=>{
-    cover2.className='selecting';
+    cover2.className='Selecting';
 })
 cover2.addEventListener('mouseleave',()=>{
     cover2.className="";
 })
 
 cover3.addEventListener('mouseover',()=>{
-    cover3.className='selecting';
+    cover3.className='Selecting';
 })
 
 cover3.addEventListener('mouseleave',()=>{
@@ -27,7 +28,7 @@ cover3.addEventListener('mouseleave',()=>{
 })
 
 cover4.addEventListener('mouseover',()=>{
-    cover4.className='selecting';
+    cover4.className='Selecting';
 })
 cover4.addEventListener('mouseleave',()=>{
     cover4.className="";
